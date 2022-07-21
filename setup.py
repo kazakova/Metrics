4316422d9d5c32bca3d1140007da5582a987fdb3
Elizaveta Kazakova <kazakova.em@phystech.edu>
2022-07-21 13:20:03 +0300

    setup file added

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name                 = 'QMetrics',
+    version              = 1.0,
+    description          = '''A utility for statistical comparison of protein identification results in proteomics and calculation of
+           quantitative metrics for total cellular response''',
+#    long_description     = (''.join(open('README.MD').readlines())),
+#    long_description_content_type="text/markdown",
+    author               = 'Elizaveta Kazakova',
+    author_email         = 'kazakova.em@phystech.edu',
+    install_requires     = ['numpy', 'pandas', 'statsmodels.sandbox.stats.multicomp', 'scipy', 'matplotlib', 'seaborn', 'scikit-learn'],
+    classifiers          = ['Intended Audience :: Science/Research',
+                            'Programming Language :: Python :: 3',
+                            'Topic :: Scientific/Engineering :: Bio-Informatics',
+                            'Topic :: Scientific/Engineering :: Chemistry',
+                            'Topic :: Scientific/Engineering :: Physics'],
+    license              = 'License :: OSI Approved :: Apache Software License',
+    packages             = find_packages(),
+    entry_points         = {'console_scripts': ['QMetrics=stattest_metrics:main']},
+    )
