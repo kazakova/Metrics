e9281203c433e4ba371e695ce4696cb5ff2e3dfc
Elizaveta Kazakova <kazakova.em@phystech.edu>
2022-08-04 15:17:32 +0300

    Add metrics output file

--- QRePS/stattest_metrics.py
+++ QRePS/stattest_metrics.py
@@ -374,11 +374,14 @@ def main():
             pi1, pi2 = metrics(quant_res, method = args.thresholds, reg_type = args.regulation, 
                                fold_change = args.fold_change, alpha = args.alpha)
             print('pi1 = {}\npi2 = {}'.format(pi1, pi2))
+            metric_df = pd.DataFrame(data = {'pi1' : [pi1], 'pi2' : [pi2]}
         else:
             e, e_mod, pi1, pi2 = metrics(quant_res, method = args.thresholds, reg_type = args.regulation,
                                         fold_change = args.fold_change, alpha = args.alpha)
             print('Euclidean distance = {}\nModified euclidean distance = {}\npi1 = {}\npi2 = {}\n'.format(e, e_mod, pi1, pi2))
-
+            metric_df = pd.DataFrame(data = {'Euclidean distance' : [e], 
+                                             'Modified euclidean distance' : [e_mod], 
+                                             'pi1' : [pi1], 'pi2' : [pi2]}
     ################# GO
         filename = path.join(args.output_dir, 'GO_network_{}.png'.format(sample_type))
         show_string_picture(genes['Gene'], filename, args.species)
@@ -388,3 +391,4 @@ def main():
         
         quant_res.to_csv(path.join(args.output_dir, 'Quant_res_{}.tsv'.format(sample_type)), sep = '\t')
         go_res.to_csv(path.join(args.output_dir, 'GO_res_{}.tsv'.format(sample_type)), sep = '\t', index = None)
+        metric_df.to_csv(path.join(args.output_dir, 'metrics_{}.tsv'.format(sample_type)), sep = '\t', index = None)
