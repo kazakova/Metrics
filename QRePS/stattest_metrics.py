a5548fd4fdd036803e24bc252064d0c5d80f7492
Elizaveta Kazakova <kazakova.em@phystech.edu>
2022-09-02 17:34:33 +0300

    GO enrichment calculation with 0 genes fixed

--- QRePS/stattest_metrics.py
+++ QRePS/stattest_metrics.py
@@ -275,8 +275,8 @@ def de_gene_list(df, method, reg_type, fold_change = 2, alpha = 0.01):
     return res[['fold_change', '-log10(fdr_BH)', 'Gene']]
 
 def show_string_picture(genes, filename, species):
-    print(genes)
     genes = genes.dropna(axis = 0)
+    print(genes)
     string_api_url = "https://string-db.org/api/"
     output_format = "image"
     method = "network"
